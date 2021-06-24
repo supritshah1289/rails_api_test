a95be796d9f8b899bf59248d596f3e53ce042437
Suprit <suprit@Suprits-MacBook-Pro.local>
2021-06-23 20:46:56 -0400

    image upload setup complete and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u1 = User.create(email: 'user@example.com', password: 'password')
-u2 = User.create(email: 'user2@example.com', password: 'password')
+# u1 = User.create(email: 'user@example.com', password: 'password')
+# u2 = User.create(email: 'user2@example.com', password: 'password')
  
-p1 = u1.posts.create(title: 'First Post', body: 'An Airplane')
-p2 = u1.posts.create(title: 'Second Post', body: 'A Train')
+# p1 = u1.posts.create(title: 'First Post', body: 'An Airplane')
+# p2 = u1.posts.create(title: 'Second Post', body: 'A Train')
  
-p3 = u2.posts.create(title: 'Third Post', body: 'A Truck')
-p4 = u2.posts.create(title: 'Fourth Post', body: 'A Boat')
+# p3 = u2.posts.create(title: 'Third Post', body: 'A Truck')
+# p4 = u2.posts.create(title: 'Fourth Post', body: 'A Boat')
  
-p3.comments.create(body: "This post was terrible", user: u1)
-p4.comments.create(body: "This post was the best thing in the whole world", user: u1)
+# p3.comments.create(body: "This post was terrible", user: u1)
+# p4.comments.create(body: "This post was the best thing in the whole world", user: u1)
+
+I1 =  User.first.image.attach(io: File.open(File.join(Rails.root, "app/assets/images/cat1.jpeg")), filename: "profile.jpeg")
